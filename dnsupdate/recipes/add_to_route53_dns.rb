5bb3d15433c93d8d41cb439155bb66bdb63c3337
Chester Burbidge <cburbidge@scottlogic.co.uk>
2018-05-10 18:15:42 +0100

    added gem

--- dnsupdate/recipes/add_to_route53_dns.rb
+++ dnsupdate/recipes/add_to_route53_dns.rb
@@ -1,6 +1,11 @@
 require 'net/http'
 include_recipe 'route53'
 
+chef_gem "nokogiri" do
+  action :install
+  version "1.5.11"
+end
+
 route53_record "create a record" do
   name  node[:opsworks][:instance][:hostname] + node[:dns_domain_end]
   value Net::HTTP.get(URI.parse('http://169.254.169.254/latest/meta-data/local-ipv4'))
